cfa936f28a88c0dc581f65935b777974e9abbabd
Simon Colohan <scolohan@sdl.com>
2026-07-23 12:19:31 +0100

    forgot to add files

---
+++ lambda/index.ts
@@ -0,0 +1,14 @@
+import { Router } from '@aws-lambda-powertools/event-handler/http';
+import type { Context } from 'aws-lambda';
+
+type ipmsEvent = { 
+}
+
+const app = new Router();
+
+const healthCheck = async () => ({ status: 'ok' });
+
+app.get('/health', healthCheck);
+
+export const handler = async (event: ipmsEvent, context: Context) =>
+  app.resolve(event, context);
